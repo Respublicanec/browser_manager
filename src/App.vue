83801a3f978ed69f535408976ef6548c8bfdbc5f
Yurii Kotov <respublicanec37@mail.ru>
2025-05-13 11:03:58 +0300

    add cards

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <script setup>
+import { ref } from "vue";
 import CardSeting from "@/components/CardSeting.vue";
 import logo from "@/assets/images/logo.svg";
 import iconSun from "@/assets/images/icon-sun.svg";
+
+const data = ref([
+  {
+    logo: "./src/assets/images/logo-devlens.svg",
+    name: "DevLens",
+    description:
+      "Quickly inspect page layouts and visualize element boundaries.",
+    isActive: true,
+  },
+  {
+    logo: "./src/assets/images/logo-style-spy.svg",
+    name: "StyleSpy",
+    description: "Instantly analyze and copy CSS from any webpage element.",
+    isActive: true,
+  },
+  {
+    logo: "./src/assets/images/logo-speed-boost.svg",
+    name: "SpeedBoost",
+    description: "Optimizes browser resource usage to accelerate page loading.",
+    isActive: false,
+  },
+  {
+    logo: "./src/assets/images/logo-json-wizard.svg",
+    name: "JSONWizard",
+    description:
+      "Formats, validates, and prettifies JSON responses in-browser.",
+    isActive: true,
+  },
+]);
 </script>
 
 <template>
   <div class="header">
     <img class="logo" :src="logo" alt="" />
-    <div class="seting_blog">
-      <img class="seting_icon" :src="iconSun" alt="" />
+    <div class="seting-blog">
+      <img class="seting-icon" :src="iconSun" alt="" />
     </div>
   </div>
-  <div class="filter_container">
-    <h2 class="header_filter">Expension list</h2>
-    <div class="buttons_filters">
+  <div class="filter-container">
+    <h2 class="header-filter">Expension list</h2>
+    <div class="buttons-filters">
       <button class="button">All</button>
       <button class="button">Active</button>
       <button class="button">Inactive</button>
     </div>
   </div>
-  <div>
-    <CardSeting />
+  <div class="list-cards">
+    <CardSeting v-for="card in data" :card="card" />
   </div>
 </template>
 
@@ -37,28 +67,28 @@ import iconSun from "@/assets/images/icon-sun.svg";
   padding: 10px;
   color: azure;
 }
-.seting_blog {
+.seting-blog {
   display: flex;
   align-items: center;
   background-color: rgb(59, 76, 118);
   border-radius: 15px;
   justify-self: end;
 }
-.seting_icon {
+.seting-icon {
   width: 25px;
   height: 25px;
   margin: 15px;
 }
-.filter_container {
+.filter-container {
   display: grid;
   grid-template-columns: 1fr 1fr;
   padding: 30px 0;
 }
-.header_filter {
+.header-filter {
   color: rgb(247, 247, 247);
   font-size: 50px;
 }
-.buttons_filters {
+.buttons-filters {
   display: flex;
   gap: 10px;
   justify-self: end;
@@ -71,5 +101,10 @@ import iconSun from "@/assets/images/icon-sun.svg";
   font-size: 20px;
   border-radius: 20px;
   border: none;
+}
+.list-cards {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 15px;
 }
 </style>
--- src/components/CardSeting.vue
+++ src/components/CardSeting.vue
@@ -1,24 +1,117 @@
 <script setup>
-import icon1 from "@/assets/images/logo-markup-notes.svg";
+import { ref } from "vue";
+
+const props = defineProps({
+  card: {
+    type: Object,
+  },
+});
+
+const emit = defineEmits(["click"]);
+
+const isActive = ref(props.card.isActive);
+
+const toggleActive = () => {
+  isActive.value = !isActive.value;
+};
 </script>
 
 <template>
-  <div class="header_card">
-    <img :src="icon1" alt="Логотип" />
-    <div>
-      <h2>DevLens</h2>
-      <span>Quickly Quickly QuicklyQuickly </span>
+  <div class="card">
+    <div class="header-card">
+      <img :src="card.logo" alt="Логотип" />
+      <div class="text-header">
+        <h2>{{ card.name }}</h2>
+        <span class="text">{{ card.description }} </span>
+      </div>
     </div>
-  </div>
-  <div>
-    <button>Remove</button>
-    <button></button>
+    <div class="footer-buttons">
+      <button class="remove">Remove</button>
+      <div class="toggle-container">
+        <input type="checkbox" id="toggle" class="toggle-input" />
+        <label for="toggle" @click="toggleActive()" class="toggle-label">
+          <span
+            class="toggle-handle"
+            :class="{ 'toggle-activ': isActive }"
+          ></span>
+        </label>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-.header_card {
+.card {
   display: grid;
-  grid-template-columns: repeat(2, 3fr);
+  gap: 20px;
+  padding: 10px 20px;
+  border: 1px solid red;
+  background-color: rgb(59, 76, 118);
+  max-width: 600px;
+  border-radius: 20px;
+}
+.header-card {
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  gap: 10px;
+  align-items: center;
+}
+.text-header {
+  color: rgb(255, 255, 255);
+}
+.text {
+}
+.footer-buttons {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 10px 0;
+}
+.remove {
+  margin-right: auto;
+  background-color: rgb(59, 76, 118);
+  border: 1px solid white;
+  color: rgb(255, 255, 255);
+  padding: 10px 20px;
+  border-radius: 20px;
+}
+.toggle-container {
+  display: grid;
+  margin-left: 100%;
+  display: flex;
+  position: relative;
+  display: inline-block;
+  width: 60px; /* Ширина переключателя */
+}
+
+.toggle-input {
+  opacity: 0;
+}
+
+.toggle-label {
+  position: absolute;
+  top: 10px;
+  right: 50px;
+  width: 56px;
+  height: 30px;
+  background-color: #c7231a;
+  display: block;
+  border-radius: 15px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.toggle-handle {
+  position: absolute;
+  top: 5px;
+  left: 3px;
+  width: 20px;
+  height: 20px;
+  background-color: white;
+  border-radius: 50%;
+  transition: transform 0.3s;
+}
+
+.toggle-activ {
+  transform: translateX(30px);
 }
 </style>
