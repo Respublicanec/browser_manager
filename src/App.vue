84d2dc3527a91ec353be88257d8a73c135721ff2
Yurii Kotov <respublicanec37@mail.ru>
2025-05-14 10:34:04 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="header">
     <img class="logo" :src="logo" alt="" />
-    <div class="seting-blog">
-      <img class="seting-icon" :src="iconSun" alt="" />
+    <div class="block" @click="colorTheme()">
+      <img class="setting-icon" :src="iconSun" alt="" />
     </div>
   </div>
+
   <div class="filter-container">
-    <h2 class="header-filter">Expension list</h2>
+    <h2 class="filter">Extension List</h2>
     <div class="buttons-filters">
       <button class="button">All</button>
       <button class="button">Active</button>
@@ -14,90 +15,97 @@
     </div>
   </div>
   <div class="list-cards">
-    <CardSeting v-for="card in data" :card="card" />
+    <ExtensionList v-for="card in data" :key="card.id" :card="card" />
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import CardSeting from "@/components/CardSeting.vue";
+import ExtensionList from "@/components/ExtensionList.vue";
 import logo from "@/assets/images/logo.svg";
 import iconSun from "@/assets/images/icon-sun.svg";
 
+const darkTheme = ref(true);
+
+function colorTheme() {
+  darkTheme.value = !darkTheme.value;
+  console.log(darkTheme.value);
+}
+
 const data = ref([
   {
-    logo: "./src/assets/images/logo-devlens.svg",
+    logo: "../src/assets/images/logo-devlens.svg",
     name: "DevLens",
     description:
       "Quickly inspect page layouts and visualize element boundaries.",
     isActive: true,
   },
   {
-    logo: "./src/assets/images/logo-style-spy.svg",
+    logo: "../src/assets/images/logo-style-spy.svg",
     name: "StyleSpy",
     description: "Instantly analyze and copy CSS from any webpage element.",
     isActive: true,
   },
   {
-    logo: "./src/assets/images/logo-speed-boost.svg",
+    logo: "../src/assets/images/logo-speed-boost.svg",
     name: "SpeedBoost",
     description: "Optimizes browser resource usage to accelerate page loading.",
     isActive: false,
   },
   {
-    logo: "./src/assets/images/logo-json-wizard.svg",
+    logo: "../src/assets/images/logo-json-wizard.svg",
     name: "JSONWizard",
     description:
       "Formats, validates, and prettifies JSON responses in-browser.",
     isActive: true,
   },
   {
-    logo: "./src/assets/images/logo-tab-master-pro.svg",
+    logo: "../src/assets/images/logo-tab-master-pro.svg",
     name: "TabMaster Pro",
     description: "Organizes browser tabs into groups and sessions.",
     isActive: true,
   },
   {
-    logo: "./src/assets/images/logo-viewport-buddy.svg",
+    logo: "../src/assets/images/logo-viewport-buddy.svg",
     name: "ViewportBuddy",
     description:
       "Simulates various screen resolutions directly within the browser.",
     isActive: false,
   },
   {
-    logo: "./src/assets/images/logo-markup-notes.svg",
+    logo: "../src/assets/images/logo-markup-notes.svg",
     name: "Markup Notes",
     description:
       "Enables annotation and notes directly onto webpages for collaborative debugging.",
     isActive: true,
   },
   {
-    logo: "./src/assets/images/logo-grid-guides.svg",
+    logo: "../src/assets/images/logo-grid-guides.svg",
     name: "GridGuides",
     description:
       "Overlay customizable grids and alignment guides on any webpage.",
     isActive: false,
   },
   {
-    logo: "./src/assets/images/logo-palette-picker.svg",
+    logo: "../src/assets/images/logo-palette-picker.svg",
     name: "Palette Picker",
     description: "Instantly extracts color palettes from any webpage.",
     isActive: true,
   },
   {
-    logo: "./src/assets/images/logo-link-checker.svg",
+    logo: "../src/assets/images/logo-link-checker.svg",
     name: "LinkChecker",
     description: "Scans and highlights broken links on any page.",
     isActive: true,
   },
   {
-    logo: "./src/assets/images/logo-dom-snapshot.svg",
+    logo: "../src/assets/images/logo-dom-snapshot.svg",
     name: "DOM Snapshot",
     description: "Capture and export DOM structures quickly.",
     isActive: false,
   },
   {
-    logo: "./src/assets/images/logo-console-plus.svg",
+    logo: "../src/assets/images/logo-console-plus.svg",
     name: "ConsolePlus",
     description:
       "Enhanced developer console with advanced filtering and logging.",
@@ -111,34 +119,36 @@ const data = ref([
   width: 100%;
   display: grid;
   grid-template-columns: repeat(2, 3fr);
-  background-color: rgb(49, 60, 90);
+  background-color: #1f2535;
   border-radius: 20px;
-  padding: 10px;
+  padding: 7px;
 }
 .logo {
   padding: 10px;
   color: azure;
 }
-.seting-blog {
+.block {
   display: flex;
   align-items: center;
-  background-color: rgb(59, 76, 118);
-  border-radius: 15px;
+  background-color: #2f354b;
+  border-radius: 10px;
   justify-self: end;
+  margin-right: 12px;
 }
-.seting-icon {
-  width: 25px;
-  height: 25px;
-  margin: 15px;
+.setting-icon {
+  width: 22px;
+  height: 22px;
+  margin: 12px;
 }
 .filter-container {
   display: grid;
   grid-template-columns: 1fr 1fr;
-  padding: 30px 0;
-}
-.header-filter {
   color: rgb(247, 247, 247);
-  font-size: 50px;
+  padding: 50px 0 10px 0;
+}
+.filter {
+  color: rgb(247, 247, 247);
+  font-size: 40px;
 }
 .buttons-filters {
   display: flex;
@@ -147,7 +157,7 @@ const data = ref([
   margin: 20px;
 }
 .button {
-  background-color: rgb(59, 76, 118);
+  background-color: #2f354b;
   padding: 10px 20px;
   color: rgb(255, 255, 255);
   font-size: 20px;
@@ -157,6 +167,6 @@ const data = ref([
 .list-cards {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  gap: 15px;
+  gap: 12px;
 }
 </style>
--- src/components/ExtensionList.vue
+++ src/components/ExtensionList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
-    <div class="header-card">
-      <img :src="card.logo" alt="Логотип" />
+    <div class="header">
+      <img class="card-logo" :src="card.logo" alt="Логотип" />
       <div class="text-header">
         <h2>{{ card.name }}</h2>
         <span class="text">{{ card.description }} </span>
@@ -11,7 +11,12 @@
       <button class="remove">Remove</button>
       <div class="toggle-container">
         <input type="checkbox" id="toggle" class="toggle-input" />
-        <label for="toggle" @click="toggleActive()" class="toggle-label">
+        <label
+          for="toggle"
+          @click="switching()"
+          class="toggle-label"
+          :class="{ 'label-red': isActive }"
+        >
           <span
             class="toggle-handle"
             :class="{ 'toggle-activ': isActive }"
@@ -35,7 +40,7 @@ const emit = defineEmits(["click"]);
 
 const isActive = ref(props.card.isActive);
 
-const toggleActive = () => {
+const switching = () => {
   isActive.value = !isActive.value;
 };
 </script>
@@ -43,23 +48,30 @@ const toggleActive = () => {
 <style>
 .card {
   display: grid;
-  gap: 20px;
-  padding: 10px 20px;
-  border: 1px solid red;
-  background-color: rgb(59, 76, 118);
+  gap: 52px;
+  padding: 12px 20px;
+  border: 1px solid #393e51;
+  background-color: #1f2535;
   max-width: 600px;
   border-radius: 20px;
 }
-.header-card {
+.header {
   display: grid;
   grid-template-columns: 1fr 5fr;
-  gap: 10px;
+  gap: 15px;
   align-items: center;
 }
 .text-header {
+  align-self: start;
+  position: relative;
   color: rgb(255, 255, 255);
 }
 .text {
+  position: absolute;
+}
+.card-logo {
+  align-self: start;
+  padding-top: 5px;
 }
 .footer-buttons {
   display: grid;
@@ -70,11 +82,12 @@ const toggleActive = () => {
 }
 .remove {
   margin-right: auto;
-  background-color: rgb(59, 76, 118);
+  background-color: #1f2533;
   border: 1px solid white;
   color: rgb(255, 255, 255);
-  padding: 10px 20px;
+  padding: 10px 15px;
   border-radius: 20px;
+  font-size: 16px;
 }
 .toggle-container {
   display: grid;
@@ -91,29 +104,32 @@ const toggleActive = () => {
 
 .toggle-label {
   position: absolute;
-  top: 3px;
-  right: 50px;
-  width: 56px;
-  height: 30px;
-  background-color: #c7231a;
+  top: 10px;
+  right: 60px;
+  width: 35px;
+  height: 20px;
+  background-color: #525867;
   display: block;
   border-radius: 15px;
   cursor: pointer;
   transition: background-color 0.3s;
 }
+.label-red {
+  background-color: #f15c55;
+}
 
 .toggle-handle {
   position: absolute;
-  top: 5px;
-  left: 3px;
-  width: 20px;
-  height: 20px;
+  top: 2px;
+  left: 2px;
+  width: 16px;
+  height: 16px;
   background-color: white;
   border-radius: 50%;
   transition: transform 0.3s;
 }
 
 .toggle-activ {
-  transform: translateX(30px);
+  transform: translateX(15px);
 }
 </style>
